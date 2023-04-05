1cdaaf2eb4735324ec0793bf97ce6e83202a88c9
PhanDuc2k3 <21011596@st.phenikaa-uni.edu.vn>
2023-04-05 23:34:17 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+//import HelloWord from './ThucHanh1/HelloWorld.js'
+// import Footer from './ThucHanh2/Header.js';
+// import Header from './ThucHanh2/Footer.js';
+import { useState } from 'react';
+const DemoComponent = (props) => {
+  return (
+    <div>
+      <div>Đây là tuổi: {props.age}</div>
+      <div>Đây là tên: {props.displayName}</div>
+    </div>
+  );
+};
+
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState('Cee');
+
+  const onIncreaseAge = () => {
+    setAge(age + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <HelloWord/> */}
+      
+      {/* <Header/>
+      <div>Hello</div>
+      <Footer/> */}
+
+  <div className='container'>
+      <div>{age}</div>
+      <div onClick={onIncreaseAge}>Tăng age lên 1</div>
+      <DemoComponent age={age} displayName={name} />
+    </div>
     </div>
   );
 }
+//ThucHanh4
+// export default App;
 
-export default App;
+
+// export default function App() {
+//   const [userInput, setUserInput] = useState("");
+//   const handleInputChange = (e) => {
+//     setUserInput(e.target.value);
+//   };
+//   const handleButtonClick = (e) => {
+//     alert(userInput)
+//   }
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         name="user"
+//         placeholder="Please type somthing"
+//         onChange={handleInputChange}
+//       />
+//       <button
+//         onClick={handleButtonClick}
+//       >Print</button>
+//     </div>
+//   );
+// }
